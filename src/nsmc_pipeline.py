cb43cb467fb428a808bd8714431cfd350e86efec
DoSungjin <tjdwls6366@gmail.com>
2022-12-06 16:39:08 +0900

    refactor : hard coding issue

--- src/nsmc_pipeline.py
+++ src/nsmc_pipeline.py
@@ -1,14 +1,18 @@
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, TextClassificationPipeline
 
 
-def get_nsmc_pipeline():
-    model = AutoModelForSequenceClassification.from_pretrained("monologg/koelectra-v3-small-nsmc")
-    tokenizer = AutoTokenizer.from_pretrained("monologg/koelectra-v3-small-nsmc")
+def get_nsmc_pipeline(
+    model_name_or_path="monologg/koelectra-v3-small-nsmc",
+    device= 0,
+    
+):
+    model = AutoModelForSequenceClassification.from_pretrained(model_name_or_path)
+    tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
     
     pipe = TextClassificationPipeline(
     model=model,
     tokenizer=tokenizer,
-    device=0,
+    device=device,
 )
 
     return pipe
