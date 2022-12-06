b54706f64cd9edbbdee03fc09b37549337f87938
DoSungjin <tjdwls6366@gmail.com>
2022-12-06 16:47:17 +0900

    chore: add comment for nsmc_pipeline func

--- src/nsmc_pipeline.py
+++ src/nsmc_pipeline.py
@@ -6,6 +6,21 @@ def get_nsmc_pipeline(
     device= 0,
     
 ):
+    """NSMC Pipeline
+    
+    Args:
+        model_name_or_path (str, optional)
+            model name (huggingface.co) or local path,
+            Defaults to "monologg/koelectra-v3-small-nsmc",
+        device (int, optional):
+            -1 for cpu, 0>= for gpu (gpu id),
+            Defaults to 0,
+            
+        Returns:
+            TextClassificationPipeline
+    """
+        
+
     model = AutoModelForSequenceClassification.from_pretrained(model_name_or_path)
     tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
     
